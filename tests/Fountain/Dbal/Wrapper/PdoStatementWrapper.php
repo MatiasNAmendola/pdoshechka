2d5902514ce09996fefd4f9bc771a8167c98c9f2
Gusakov Nikita <nikita@hell0.ru>
2013-08-04 19:41:12 +0400

    make test wrapper not abstract

--- tests/Fountain/Dbal/Wrapper/PdoStatementWrapper.php
+++ tests/Fountain/Dbal/Wrapper/PdoStatementWrapper.php
@@ -2,6 +2,39 @@
 
 namespace Fountain\Dbal\Wrapper;
 
-abstract class PdoStatementWrapper
+class PdoStatementWrapper
 {
+    public $queryString;
+    private $_driverOptions;
+    private $_query;
+    private $_executed;
+    private $_values;
+    private $_fetchData = array();
+
+    public function __construct($query, array $options)
+    {
+        $this->_query = $this->queryString = $query;
+        $this->_driverOptions = $options;
+    }
+
+    public function execute(array $params = array())
+    {
+        $this->_executed[] = array(
+            'params' => $params,
+            'query'  => $this->_query
+        );
+    }
+
+    public function bindValue($param, $value, $type = \PDO::PARAM_STR)
+    {
+        $this->_values[$param] = array(
+            'value' => $value,
+            'type'  => $type
+        );
+    }
+
+    public function setFetchData(array $data)
+    {
+        $this->_fetchData = $data;
+    }
 }
