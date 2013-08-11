3a8367197b174bcf1ba915033dfcb893f59c8fa5
Gusakov Nikita <nikita@hell0.ru>
2013-08-11 21:32:24 +0400

    add pdo tests

--- tests/Fountain/Dbal/Tests/PdoTest.php
+++ tests/Fountain/Dbal/Tests/PdoTest.php
@@ -6,10 +6,45 @@ use Fountain\Dbal\Pdo;
 
 class PdoTest extends \PHPUnit_Framework_TestCase
 {
+    public function testSimpleConstruct()
+    {
+        $pdo = new Pdo(array(
+            'driver' => 'sqlite',
+            'memory' => true
+        ));
+        $this->assertSame('sqlite::memory:', $pdo->getDsn());
+    }
+
+    public function testMysqlConstruct()
+    {
+        $this->markTestSkipped('The test is not yet written');
+    }
+
+    public function testPostgresConstruct()
+    {
+        $this->markTestSkipped('The test is not yet written');
+    }
+
     public function testConstructFromReadyDsnString()
     {
         $pdo = new Pdo('sqlite::memory:');
 
         $this->assertSame('sqlite::memory:', $pdo->getDsn());
+    }
+
+    public function testPrepare()
+    {
+        $pdo = new Pdo('');
+        $stmt = $pdo->prepare('SELECT * FROM test');
+
+        $this->assertInstanceOf('Fountain\\Dbal\\PdoStatement', $stmt);
+        $this->assertSame('SELECT * FROM test', $stmt->queryString);
+    }
+
+    public function testInvoke()
+    {
+        $pdo = new Pdo('');
+
+        $this->assertInstanceOf('Fountain\\Dbal\\PdoStatement', $pdo(''));
     }
 }
